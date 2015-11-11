067428881a7504dfeefaf3d48e924b214f85044d
Sami Ylonen <sami.ylonen@gmail.com>
2015-11-11 22:58:23 +0100

    unracked files

---
+++ KeySndr.Base/Commands/GetViewConfigurations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KeySndr.Base.Providers;
+using KeySndr.Common;
+
+namespace KeySndr.Base.Commands
+{
+    public class GetViewInputConfigurations : ICommand<ApiResult<IEnumerable<string>>>
+    {
+        private readonly IInputConfigProvider inputConfigProvider;
+        public ApiResult<IEnumerable<string>> Result { get; private set; }
+
+        public GetViewInputConfigurations(IInputConfigProvider p)
+        {
+            inputConfigProvider = p;
+        }
+
+        public void Execute()
+        {
+            try
+            {
+                Result = new ApiResult<IEnumerable<string>>
+                {
+                    Content = inputConfigProvider.Configs.Where(i => i.HasView).Select(i => i.Name),
+                    Success = true
+                };
+            }
+            catch (Exception e)
+            {
+                Result = new ApiResult<IEnumerable<string>>
+                {
+                    Success = false,
+                    Message = "Failed to get input configurations",
+                    ErrorMessage = e.Message
+                };
+            }
+        }
+    }
+}
---
+++ KeySndr.Base/Controllers/SettingsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using KeySndr.Base.Commands;
+using KeySndr.Base.Domain;
+using KeySndr.Base.Providers;
+using KeySndr.Common;
+
+namespace KeySndr.Base.Controllers
+{
+    public class SettingsController : ApiController
+    {
+        private readonly IAppConfigProvider appConfigProvider;
+        private readonly IFileSystemProvider fileSystemProvider;
+
+        public SettingsController()
+        {
+            appConfigProvider = ObjectFactory.GetProvider<IAppConfigProvider>();
+            fileSystemProvider = ObjectFactory.GetProvider<IFileSystemProvider>();
+        }
+
+        public SettingsController(IFileSystemProvider p, IAppConfigProvider a)
+        {
+            fileSystemProvider = p;
+            appConfigProvider = a;
+        }
+
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        [HttpGet]
+        public ApiResult<AppConfig> GetAppSettings()
+        {
+            var cmd = new GetAppSettings(appConfigProvider);
+            cmd.Execute();
+            return cmd.Result;
+        }
+
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        [HttpPost]
+        public ApiResult<Object> StoreAppSettings(AppConfig c)
+        {
+            var cmd = new StoreAppConfig(fileSystemProvider, appConfigProvider, c);
+            cmd.Execute();
+            return cmd.Result;
+        }
+    }
+}
