1805169920a7087fe7c6ca83dd48c33f56776373
Sami Ylonen <sami.ylonen@gmail.com>
2015-11-21 22:24:32 +0100

    missing files

---
+++ KeySndr.Win/Events/LogEntryEventArgs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace KeySndr.Win.Events
+{
+    public class LogEntryEventArgs : EventArgs
+    {
+        public string Message { get; private set; }
+        public LogEntryEventArgs(string m)
+            : base()
+        {
+            Message = m;
+        }
+    }
+
+    public class InfoLogEntryEventArgs : LogEntryEventArgs
+    {
+        public InfoLogEntryEventArgs(string m)
+            : base(m)
+        { 
+        }
+    }
+
+    public class DebugLogEntryEventArgs : LogEntryEventArgs
+    {
+        public DebugLogEntryEventArgs(string m)
+            : base(m)
+        {
+        }
+    }
+
+    public class ErrorLogEntryEventArgs : LogEntryEventArgs
+    {
+        public Exception Exception { get; private set; }
+        public ErrorLogEntryEventArgs(string m, Exception e)
+            : base(m)
+        {
+            Exception = e;
+        }
+    }
+}
---
+++ KeySndr.Win/Providers/LoggingProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using KeySndr.Base.Providers;
+using KeySndr.Win.Events;
+
+namespace KeySndr.Win.Providers
+{
+    public class LoggingProvider : ILoggingProvider
+    {
+        public EventHandler<LogEntryEventArgs> OnLogEntry;
+
+        public void Dispose()
+        {
+
+        }
+
+        public void Debug(string m)
+        {
+            OnLogEntry?.Invoke(this, new DebugLogEntryEventArgs(m));
+        }
+
+        public void Info(string m)
+        {
+            OnLogEntry?.Invoke(this, new InfoLogEntryEventArgs(m));
+        }
+
+        public void Error(string m, Exception e)
+        {
+            OnLogEntry?.Invoke(this, new ErrorLogEntryEventArgs(m, e));
+        }
+    }
+}
