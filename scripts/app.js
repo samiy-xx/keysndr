17bcb8206a607a794d302e1922795e36479c10e5
Sami Ylonen <sami.ylonen@gmail.com>
2015-11-26 20:18:35 +0100

    some

---
+++ scripts/app.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module("keysndrsite", []);
+
+app.config(['$locationProvider',
+    function ($locationProvider) {
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    }
+]);
+
+// Prototypes
+Array.prototype.insertAt = function (index, item) {
+    this.splice(index, 0, item);
+};
+
+Array.prototype.removeAt = function (index) {
+    this.splice(index, 1);
+};
---
+++ scripts/app/controllers/site-controller.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+
+    app.controller('siteController', ["$scope", "$location", SiteController]);
+
+    function SiteController(scope, $location, service) {
+        
+    }
+})(app);
