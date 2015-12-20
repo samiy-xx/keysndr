576a68ffe408555e5cfd1607b7de95b50f501a8f
Sami Ylonen <sami.ylonen@gmail.com>
2015-12-20 11:49:40 +0100

    couple tests

---
+++ KeySndr.Base.Tests/CommandTests/GetAllScriptObjects_Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KeySndr.Base.Commands;
+using KeySndr.Base.Domain;
+using KeySndr.Base.Providers;
+using KeySndr.Common;
+using Moq;
+using NUnit.Framework;
+
+namespace KeySndr.Base.Tests.CommandTests
+{
+    [TestFixture]
+    public class GetAllScriptObjects_Tests
+    {
+        private IScriptProvider scriptProvider;
+        private Mock<IScriptProvider> scriptProviderMock;
+
+        private GetAllScriptObjects cmd;
+        [SetUp]
+        public void Setup()
+        {
+            scriptProviderMock = new Mock<IScriptProvider>();
+            scriptProviderMock.Setup(s => s.Scripts).Returns(new List<InputScript>
+            {
+                new InputScript { FileName = "test.js"},
+                new InputScript {FileName = "test2.js"}
+            });
+            scriptProvider = scriptProviderMock.Object;
+            cmd = new GetAllScriptObjects(scriptProvider);
+        }
+
+        [Test]
+        public void Test()
+        {
+            cmd.Execute();
+            scriptProviderMock.Verify(s => s.Scripts, Times.Once);
+
+            Assert.IsNotNull(cmd);
+            Assert.IsNotNull(cmd.Result);
+            Assert.IsInstanceOf<ApiResult<IEnumerable<InputScript>>>(cmd.Result);
+            Assert.IsInstanceOf<List<InputScript>>(cmd.Result.Content.ToList());
+            Assert.AreEqual(2, cmd.Result.Content.Count());
+        }
+    }
+}
---
+++ KeySndr.Base.Tests/CommandTests/GetAppSettings_Tests.cs
@@ -0,0 +1,37 @@
+﻿using KeySndr.Base.Commands;
+using KeySndr.Base.Domain;
+using KeySndr.Base.Providers;
+using KeySndr.Common;
+using Moq;
+using NUnit.Framework;
+
+namespace KeySndr.Base.Tests.CommandTests
+{
+    [TestFixture]
+    public class GetAppSettings_Tests
+    {
+        private IAppConfigProvider provider;
+        private Mock<IAppConfigProvider> provicerMock;
+        private GetAppSettings cmd;
+
+        [SetUp]
+        public void Setup()
+        {
+            provicerMock = new Mock<IAppConfigProvider>();
+            provicerMock.Setup(p => p.AppConfig).Returns(new AppConfig());
+            provider = provicerMock.Object;
+            cmd = new GetAppSettings(provider);
+        }
+
+        [Test]
+        public void ReturnAppConfig()
+        {
+            cmd.Execute();
+            var result = cmd.Result;
+            provicerMock.Verify(p => p.AppConfig, Times.Once);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<ApiResult<AppConfig>>(result);
+        }
+    }
+}
