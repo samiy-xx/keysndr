2c3906b0fcd323f7432aefe14a50c062a9b8038e
Sami Ylonen <sami.ylonen@gmail.com>
2016-01-19 19:13:20 +0100

    couple tests and Ui work

---
+++ KeySndr.Base.Tests/CommandTests/GetAllInputConfigurationItems_Tests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using KeySndr.Base.Commands;
+using KeySndr.Base.Providers;
+using KeySndr.Common;
+using Moq;
+using NUnit.Framework;
+
+namespace KeySndr.Base.Tests.CommandTests
+{
+    [TestFixture]
+    public class GetAllInputConfigurationItems_Tests
+    {
+        private Mock<IInputConfigProvider> inputConfigProviderMock;
+        private IInputConfigProvider inputConfigProvider;
+        private GetAllInputConfigurationItems cmd;
+
+        [SetUp]
+        public void Setup()
+        {
+            inputConfigProviderMock = new Mock<IInputConfigProvider>();
+            inputConfigProviderMock.Setup(s => s.Configs).Returns(TestConfigs);
+            inputConfigProvider = inputConfigProviderMock.Object;
+            cmd = new GetAllInputConfigurationItems(inputConfigProvider);
+        }
+
+        [Test]
+        public void Executes()
+        {
+            cmd.Execute();
+            var result = cmd.Result;
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Content);
+            Assert.IsTrue(result.Success);
+            Assert.IsInstanceOf<IEnumerable<ConfigurationListItem>>(result.Content);
+            inputConfigProviderMock.Verify(i => i.Configs, Times.Once);
+        }
+
+        private IEnumerable<InputConfiguration> TestConfigs()
+        {
+            return new List<InputConfiguration>
+            {
+                TestFactory.CreateTestInputConfiguration()
+            };
+        } 
+    }
+}
---
+++ KeySndr.Base.Tests/CommandTests/GetLegacyConfigurations_Tests.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using KeySndr.Base.Commands;
+using KeySndr.Base.Providers;
+using KeySndr.Common;
+using Moq;
+using NUnit.Framework;
+
+namespace KeySndr.Base.Tests.CommandTests
+{
+    [TestFixture]
+    public class GetLegacyConfigurations_Tests
+    {
+        private Mock<IInputConfigProvider> inputConfigProviderMock;
+        private IInputConfigProvider inputConfigProvider;
+        private GetLegacyInputConfigurations cmd;
+
+        [SetUp]
+        public void Setup()
+        {
+            inputConfigProviderMock = new Mock<IInputConfigProvider>();
+            inputConfigProviderMock.Setup(s => s.Configs).Returns(TestConfigs);
+            inputConfigProvider = inputConfigProviderMock.Object;
+            cmd = new GetLegacyInputConfigurations(inputConfigProvider);
+        }
+
+        [Test]
+        public void Executes()
+        {
+            cmd.Execute();
+            var result = cmd.Result;
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Content);
+            Assert.IsTrue(result.Success);
+            Assert.IsInstanceOf<IEnumerable<string>>(result.Content);
+            Assert.AreEqual(2, result.Content.Count());
+            Assert.AreEqual("test1", result.Content.First());
+            Assert.AreEqual("test3", result.Content.Last());
+            inputConfigProviderMock.Verify(i => i.Configs, Times.Once);
+        }
+
+        private IEnumerable<InputConfiguration> TestConfigs()
+        {
+            return new List<InputConfiguration>
+            {
+                new InputConfiguration
+                {
+                    Name = "test1"
+                },
+                new InputConfiguration
+                {
+                    Name = "test2",
+                    View = "test"
+                },
+                new InputConfiguration
+                {
+                    Name = "test3"
+                }
+            };
+        }
+    }
+}
