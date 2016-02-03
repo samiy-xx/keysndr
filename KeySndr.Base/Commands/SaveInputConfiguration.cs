0d4dddcdd561f907a43c34bef0038073f8a33265
Sami Ylonen <sami.ylonen@gmail.com>
2016-02-03 21:55:45 +0100

    small fix

--- KeySndr.Base/Commands/SaveInputConfiguration.cs
+++ KeySndr.Base/Commands/SaveInputConfiguration.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using KeySndr.Base.Providers;
 using KeySndr.Common;
+using KeySndr.Common.Extensions;
 
 namespace KeySndr.Base.Commands
 {
@@ -68,7 +69,7 @@ namespace KeySndr.Base.Commands
 
         private void CreateMediaFolder()
         {
-            storageProvider.CreateMediaFolder(configuration.Name);
+            storageProvider.CreateMediaFolder(configuration.Name.RemoveWhitespace());
         }
     }
 }
--- KeySndr.Base/Providers/FileStorageProvider.cs
+++ KeySndr.Base/Providers/FileStorageProvider.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using KeySndr.Base.Domain;
 using KeySndr.Common;
 using KeySndr.Common.Extensions;
